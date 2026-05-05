2b8465d68d765bc6bd71bc9532ea5e473a4f17e7
Shaun Hossain <shaun.mononsoft@gmail.com>
2026-05-05 20:22:32 +0600

    organizing components

---
+++ app/organized/page.tsx
@@ -0,0 +1,11 @@
+import HeroSection from '@/features/hero-section/hero-section';
+
+const page = () => {
+    return (
+        <div>
+            <HeroSection />
+        </div>
+    );
+};
+
+export default page;
---
+++ components/layout/navbar.tsx
@@ -0,0 +1,41 @@
+import { Button } from '../ui/button';
+
+export const Navbar = () => {
+    return (
+        <nav className="flex justify-between w-full font-montserrat border-3 border-indigo-600">
+            <h1 className="text-md text-white uppercase font-bold flex flex-col items-center leading-tight">
+                <span className="text-[9px] font-regular">The</span>
+                <span>Podmium</span>
+                <span>Mindset</span>
+            </h1>
+            <ul className="text-white uppercase flex gap-8 items-center">
+                <li className="group relative cursor-pointer px-6 py-2 overflow-hidden transition-colors duration-500 hover:text-black">
+                    <span className="relative z-10">Home</span>
+
+                    {/* 2. The background layer */}
+                    <div className="absolute inset-0 bg-white scale-x-0 origin-right transition-transform duration-500 group-hover:scale-x-100 group-hover:origin-left" />
+                </li>
+                <li className="group relative cursor-pointer px-6 py-2 overflow-hidden transition-colors duration-500 hover:text-black">
+                    <span className="relative z-10">Programs</span>
+                    <div className="absolute inset-0 bg-white scale-x-0 origin-right transition-transform duration-500 group-hover:scale-x-100 group-hover:origin-left" />
+                </li>
+                <li className="group relative cursor-pointer px-6 py-2 overflow-hidden transition-colors duration-500 hover:text-black">
+                    <span className="relative z-10">About</span>
+                    <div className="absolute inset-0 bg-white scale-x-0 origin-right transition-transform duration-500 group-hover:scale-x-100 group-hover:origin-left" />
+                </li>
+                <li className="group relative cursor-pointer px-6 py-2 overflow-hidden transition-colors duration-500 hover:text-black">
+                    <span className="relative z-10">Contact</span>
+                    <div className="absolute inset-0 bg-white scale-x-0 origin-right transition-transform duration-500 group-hover:scale-x-100 group-hover:origin-left" />
+                </li>
+                <li>
+                    <Button
+                        variant="outline"
+                        className="text-black w-60 uppercase rounded-none cursor-pointer"
+                    >
+                        Book a call
+                    </Button>
+                </li>
+            </ul>
+        </nav>
+    );
+};
